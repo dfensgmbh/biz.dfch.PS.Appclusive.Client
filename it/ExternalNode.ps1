77a4c56d59ca415476489640805b738ad755d578
Paschalis Bimpisidis <pbimpisidis@gmail.com>
2016-08-18 13:51:27 +0200

    * function Create-ExternalNodeBag added
    #59

--- it/ExternalNode.ps1
+++ it/ExternalNode.ps1
@@ -1,4 +1,48 @@
-# This file intentionally left blank
+function Create-ExternalNodeBag {
+	Param
+	(
+		$Svc
+		,
+		$Name
+		,
+		$Description = "Test External Node Bag"
+		,
+		$Value = [biz.dfch.CS.Appclusive.Public.Constants+EntityKindId]::EntityBag.value__
+		,
+		[Parameter(Mandatory=$true)] $ExternalNodeId
+		,
+		$Tid = (Get-ApcTenant -Current).Id
+	)
+	
+	#add parameters
+	$newExternalNodeBag = New-Object biz.dfch.CS.Appclusive.Api.Core.ExternalNodeBag;
+	$newExternalNodeBag.Name = $name;
+	$newExternalNodeBag.Description = $description;
+	$newExternalNodeBag.Value = $value;
+	$newExternalNodeBag.ExternaldNodeId = $externalNodeId;
+	$newExternalNodeBag.Tid = $tid;
+	
+	#ACT create external node bag
+	$svc.Core.AddToExternalNodeBags($newExternalNodeBag);
+	$result = $svc.Core.SaveChanges();
+	
+	#get external node bag
+	$query = "Id eq {0}" -f $newExternalNodeBag.Id;
+	$externalNodeBag = $svc.Core.ExternalNodeBags.AddQueryOption('$filter', $query) | select;
+	
+	#ASSERT external node bag
+	$bin = $result.StatusCode | Should Be 201;
+	$bin = $externalNodeBag | Should BeOfType [biz.dfch.CS.Appclusive.Api.Core.ExternalNodeBag];
+	$bin = $externalNodeBag | Should Not Be $null;
+	$bin = $externalNodeBag.Name = $name;
+	$bin = $externalNodeBag.Description = $description;
+	$bin = $externalNodeBag.Value = $value;
+	$bin = $externalNodeBag.ExternaldNodeid = $externalNodeId;
+	$bin = $externalNodeBag.Tid = $tid;
+
+	return $externalNodeBag;
+	
+}
 
 #
 # Copyright 2016 d-fens GmbH
