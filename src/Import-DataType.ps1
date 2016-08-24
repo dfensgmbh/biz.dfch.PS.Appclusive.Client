182b7738fb4da16018740d70d5cea61a05b23f12
Ronald Rink <ronald.rink@d-fens.net>
2016-08-24 20:39:13 +0200

    Import-DataType
    * simplified data type lookup
    * added missing atrtibutes
    * adjusted renamed attributes
    * improved error logging
    * removed module infix
    
    #64

--- src/Import-DataType.ps1
+++ src/Import-DataType.ps1
@@ -2,60 +2,69 @@ function Import-DataType {
 <#
 .SYNOPSIS
 Creates or Recreates DataTypes for an EntityKind (based on .NET Class)
+
 .DESCRIPTION
 Creates or Recreates DataTypes for an EntityKind (based on .NET Class)
 
-Inspects the .NET Class for all it's properties and will create for every Property with an EntityBag Mapping a DataType. All supported Validation Attributes will be parsed into the DataTypes.
-.OUTPUTS
-default | json | json-pretty | xml | xml-pretty | PSCredential | Clear
-.EXAMPLE
-Import-DataType -EntityKindVersion biz.dfch.Appclusive.Products.Infrastructure.VirtualMachine.Network.NicCollection.Nic -RecreateIfExist
+Inspects the .NET Class for all its properties and will create for every Property with an EntityBag Mapping a DataType. All supported Validation Attributes will be parsed into the DataTypes.
 
-EntityKindId      : 15194
-ValidateSet       :
-ValidatePattern   :
-ValidateScript    :
-Type              : System.String
-Default           :
-Minimum           :
-Maximum           :
-Increment         :
-IncrementFunction :
-IsRequired        : True
-Unit              :
-Id                : 204
-Tid               : 11111111-1111-1111-1111-111111111111
-Name              : biz.dfch.Appclusive.Products.Infrastructure.VirtualMachine.Network.NicCollection.Nic.Name
-Description       : This string property specifies the name of the virtual Network Interface Card
-CreatedById       : 1
-ModifiedById      : 1
-Created           : 22.08.2016 11:45:01 +02:00
-Modified          : 22.08.2016 11:45:01 +02:00
-RowVersion        : {0, 0, 0, 0...}
-EntityKind        :
-Tenant            :
-CreatedBy         :
-ModifiedBy        :
+.OUTPUTS
+default | json | json-pretty | xml | xml-pretty
+
+.EXAMPLE
+# Imports all properties of the product 'biz.dfch.Appclusive.Products.Infrastructure.V001.VirtualMachine'. Existing properties will be deleted if they already exist.
+Import-DataType -EntityKindVersion biz.dfch.Appclusive.Products.Infrastructure.V001.VirtualMachine -RecreateIfExist
+
+EntityKindId       : 4099
+ValidateSet        :
+ValidatePattern    :
+ValidateScript     :
+Type               : System.Double
+KeyNameValueFilter :
+Default            : 4
+Minimum            : 0.5
+Maximum            : 384
+Increment          : 0.1
+IncrementFunction  :
+IsRequired         : True
+Unit               : GB
+Id                 : 88
+Tid                : 11111111-1111-1111-1111-111111111111
+Name               : biz.dfch.Appclusive.Products.Infrastructure.VirtualMachine.Memory.Size
+Description        : This property specifies the memory in MB of the virtual machine.
+CreatedById        : 3
+ModifiedById       : 3
+Created            : 8/24/2016 8:31:04 PM +02:00
+Modified           : 8/24/2016 8:31:04 PM +02:00
+RowVersion         : {0, 0, 0, 0...}
+EntityKind         :
+Tenant             :
+CreatedBy          :
+ModifiedBy         :
+
+# output suppressed
+
 .LINK
 Online Version: http://dfch.biz/biz/dfch/PS/Appclusive/Client/Import-DataType/
+
 .NOTES
 See module manifest for dependencies and further requirements.
 #>
 [CmdletBinding(
-    SupportsShouldProcess = $false
+    SupportsShouldProcess = $true
 	,
-    ConfirmImpact = 'Low'
+    ConfirmImpact = 'Medium'
 	,
 	HelpURI = 'http://dfch.biz/biz/dfch/PS/Appclusive/Client/Import-DataType/'
 )]
 Param 
 (
-	# Specifies the name of the entity to be parsed
-	[Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+	# Specifies the Version of the EntityKind to be imported
+	[Parameter(Mandatory = $true, Position = 0)]
     [Alias("FQCN")]
 	[string] $EntityKindVersion
 	,
-	# Specifies to delete and create the datatypes if they already exist
+	# Specifies to delete already existing entities during import
 	[Parameter(Mandatory = $false, Position = 1)]
 	[Alias("f")]
 	[Alias("force")]
@@ -81,7 +90,7 @@ Begin
 
 	$datBegin = [datetime]::Now;
 	[string] $fn = $MyInvocation.MyCommand.Name;
-	Log-Debug -fn $fn -msg ("CALL. svc '{0}'. Name '{1}'." -f ($svc -is [Object]), $EntityKindVersion) -fac 1;
+	Log-Debug -fn $fn -msg ("CALL. svc '{0}'. EntityKindVersion '{1}'." -f ($svc -is [Object]), $EntityKindVersion) -fac 1;
 
 	# Parameter validation
 	Contract-Requires ($svc.Core -is [biz.dfch.CS.Appclusive.Api.Core.Core]) "Connect to the server before using the Cmdlet"
@@ -95,76 +104,80 @@ Process
     # Return values are always and only returned via OutputParameter.
     $OutputParameter = $null;
 
-    try
-    {
-        $entityKind = Get-ApcEntityKind -Version $EntityKindVersion -svc $svc;
-        Contract-Assert $entityKind "EntityKindVersion does not exist";
-        Log-Debug $fn ("Loaded EntityKind with Version:{0} and Id:{1}" -f $entityKind.Version, $entityKind.Id);
+	$entityKind = biz.dfch.PS.Appclusive.Client\Get-ApcEntityKind -Version $EntityKindVersion -svc $svc;
+	Contract-Assert (!!$entityKind) "EntityKindVersion does not exist";
+	Log-Debug $fn ("Found EntityKind.Version:{0} [{1}]" -f $entityKind.Version, $entityKind.Id);
 
-        $EntityType = LoadTypeFromLocalAssemblybyFullQualifiedClassName $entityKind.Version;
-        $ClassProperties = $EntityType.GetProperties();
-        
-        $r = @();
-        
-        $dataTypes = $svc.Diagnostics.DataTypes.AddQueryOption('$filter', ("EntityKindId eq {0}" -f $entityKind.Id));
-        if ($PSBoundParameters.ContainsKey('RecreateIfExist'))
-        {
-            ForEach ($dataType in $dataTypes)
-            {
-                Remove-ApcEntity -svc $svc -InputObject $dataType -Confirm:$false;
-            }
+	$entityFqcn = biz.dfch.PS.System.Utilities\Get-DataType $entityKind.Version;
+	Contract-Assert (1 -eq $entityFqcn.Count)
+	$instance = New-Object $entityFqcn;
+	Contract-Assert (!!$instance)
+	$properties = $instance.GetType().GetProperties([System.Reflection.BindingFlags]::Public -bor [System.Reflection.BindingFlags]::FlattenHierarchy -bor [System.Reflection.BindingFlags]::Instance);
+	Contract-Assert (!!$properties);
+	Contract-Assert ($properties -is [Array])
+	
+	$r = @();
+	
+	$q = "EntityKindId eq {0}" -f $entityKind.Id;
+	$dataTypes = $svc.Diagnostics.DataTypes.AddQueryOption('$filter', $q) | Select;
+	if ($PSBoundParameters.ContainsKey('RecreateIfExist'))
+	{
+		foreach ($dataType in $dataTypes)
+		{
+			biz.dfch.PS.Appclusive.Client\Remove-Entity -svc $svc -InputObject $dataType -Confirm:$false;
+		}
 
-            $dataTypes = @();
-        }
+		$dataTypes = @();
+	}
 
-        ForEach ($classProperty in $ClassProperties)
-        {
-            $hasEntityBagAttribute = HasEntityBagAttribute $classProperty;
-            if (!$hasEntityBagAttribute)
-            {
-                continue;
-            }
+	foreach ($classProperty in $properties)
+	{
+		$hasEntityBagAttribute = HasEntityBagAttribute $classProperty;
+		if (!$hasEntityBagAttribute)
+		{
+			continue;
+		}
 
-            $dataType = $dataTypes | where { $_.Name -eq $classProperty.Name };
-            if (!!$dataType)
-            {
-                # Do not change an existing dataType
-                Log-Debug $fn ("[{0}]::{1} has not been updated." -f $entityKind.Version,$classProperty.Name);
-                continue;
-            }
+		$dataType = $dataTypes |? Name -eq $classProperty.Name;
+		if (!!$dataType)
+		{
+			# Do not change an existing dataType
+			Log-Warning $fn ("{0}.{1} has not been updated." -f $entityKind.Version, $classProperty.Name);
+			continue;
+		}
 
-            $dataType = New-Object biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType;
-            $svc.Diagnostics.AddToDataTypes($dataType);
+		$dataType = New-Object biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType;
+		$svc.Diagnostics.AddToDataTypes($dataType);
 
-            $dataType.EntityKindId = $entityKind.Id;
-            $dataType.Type = $classProperty.PropertyType.FullName;
-            
-            ApplyEntityBagAttribute $dataType $classProperty;
-            ApplyPropertyDescriptionAttribute $dataType $classProperty;
-            ApplyDefaultValue $dataType $classProperty;
-            ApplyRequired $dataType $classProperty;
-            ApplyRange $dataType $classProperty;
-            ApplyIncrement $dataType $classProperty;
-            ApplyUnit $dataType $classProperty;
-            ApplyValidatePattern $dataType $classProperty;
-            ApplyValidateScript $dataType $classProperty;
-            ApplyValidateSet $dataType $classProperty;
-            
-	        $svc.Diagnostics.UpdateObject($dataType);
-            $null = $svc.Diagnostics.SaveChanges();
+		$dataType.EntityKindId = $entityKind.Id;
+		$dataType.Type = $classProperty.PropertyType.FullName;
+		
+		ApplyEntityBagAttribute $dataType $classProperty;
+		ApplyPropertyDescriptionAttribute $dataType $classProperty;
+		ApplyDefaultValue $dataType $classProperty;
+		ApplyRequired $dataType $classProperty;
+		ApplyRange $dataType $classProperty;
+		ApplyIncrement $dataType $classProperty;
+		ApplyUnit $dataType $classProperty;
+		ApplyValidatePattern $dataType $classProperty;
+		ApplyValidateScript $dataType $classProperty;
+		ApplyValidateSet $dataType $classProperty;
+		ApplyKeyNameValueFilter $dataType $classProperty;
+		ApplyStringLength $dataType $classProperty;
+		
+		$svc.Diagnostics.UpdateObject($dataType);
+		if($PSCmdlet.ShouldProcess(($dataType | Out-String)))
+		{
+			$null = $svc.Diagnostics.SaveChanges();
+			$r += $dataType;
+		}
 
-            $r += $dataType;
-        }
+	}
 
-	    $OutputParameter = Format-ResultAs $r $As;
-	    $fReturn = $true;
-    }
-    catch
-    {
-        Write-Error ($error[0]);
-        $OutputParameter = $null;
-	    $fReturn = $false;
-    }
+	# $OutputParameter = biz.dfch.PS.Appclusive.Client\Format-ResultAs $r $As;
+	$OutputParameter = $r;
+	$fReturn = $true;
+
 }
 # Process
 
@@ -183,161 +196,156 @@ End
 
 if($MyInvocation.ScriptName) { Export-ModuleMember -Function Import-DataType; } 
 
-
-function LoadTypeFromLocalAssemblybyFullQualifiedClassName([string] $FQCN)
-{
-    $assemblies = [System.AppDomain]::CurrentDomain.GetAssemblies();
-    $foundClasses = @();
-
-	foreach($assembly in $assemblies)
-	{		
-		foreach($definedType in $assembly.DefinedTypes)
-		{
-			# only filter public and nested classes, skip interfaces
-			if(!(($definedType.IsPublic -eq $true -Or $definedType.IsNestedPublic -eq $true) -And $definedType.IsInterface -ne $true))
-			{
-				continue;
-			}
-			
-			$definedTypeFullName = $definedType.FullName;
-			# swap base type into type name to enable search
-			if($BaseType)
-			{
-				$definedTypeFullName = $definedType.BaseType.FullName;
-			}
-
-			if($definedTypeFullName -cne $FQCN)
-			{
-				continue;
-			}
-			
-			# swap original type name back (because of BaseType switch)
-			$definedTypeFullName = $definedType.FullName;
-
-            $foundClasses += $definedType;
-		}
-	}
-
-    if ($foundClasses.Count -eq 1)
-    {
-        return $foundClasses[0];
-    }
-    elseif ($foundClasses.Count -eq 0)
-    {
-        throw ("'{0}' is not in a loaded assembly" -f $FQCN);
-    }
-    else 
-    {
-        throw ("'{0}' is not Unique" -f $FQCN);
-    }
-}
-
 function HasEntityBagAttribute($property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Converters.EntityBagAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Converters.EntityBagAttribute], $true);
     
     return ($attr.Count -eq 1);
 }
 
-function ApplyEntityBagAttribute([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyEntityBagAttribute([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Converters.EntityBagAttribute], $true);
-    
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Converters.EntityBagAttribute], $true);
+    Contract-Assert (!!$attr) "EntityBagAttribute does not exist $($dataType.Type)"
+	
     $dataType.Name = $attr.Name;
 }
 
-function ApplyPropertyDescriptionAttribute([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyPropertyDescriptionAttribute([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.EntityBagDescriptionAttribute], $true);
-    if ($attr)
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.EntityBagDescriptionAttribute], $true);
+
+    if (!$attr)
     {
-        $dataType.Description = $attr.Name;
+		return;
     }
+
+	$dataType.Description = $attr.Name;
 }
 
-function ApplyDefaultValue([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyDefaultValue([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([System.ComponentModel.DefaultValueAttribute], $true);
-    if ($attr)
+    $attr = $property.GetCustomAttributes([System.ComponentModel.DefaultValueAttribute], $true);
+
+    if (!$attr)
     {
-        $dataType.Default = $attr.Value;
+		return;
     }
+
+	$dataType.Default = $attr.Value;
 }
 
-function ApplyRequired([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyRequired([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([System.ComponentModel.DataAnnotations.RequiredAttribute], $true);
+    $attr = $property.GetCustomAttributes([System.ComponentModel.DataAnnotations.RequiredAttribute], $true);
 
-    if ($attr)
+	$dataType.IsRequired = $false;
+    
+	if (!$attr)
     {
-        $dataType.IsRequired = $true;
+		return;
     }
-    else
-    {
-        $dataType.IsRequired = $false;
-    }
+	
+	$dataType.IsRequired = $true;
 }
 
-function ApplyRange([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyRange([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([System.ComponentModel.DataAnnotations.RangeAttribute], $true);
+    $attr = $property.GetCustomAttributes([System.ComponentModel.DataAnnotations.RangeAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.Minimum = $attr.Minimum;
-        $dataType.Maximum = $attr.Maximum;
+		return;
     }
+
+	$dataType.Minimum = $attr.Minimum;
+	$dataType.Maximum = $attr.Maximum;
 }
 
-function ApplyIncrement([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyIncrement([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.IncrementAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.IncrementAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.Increment = $attr.Increment;
-        $dataType.IncrementFunction = $attr.IncrementFunction;
+		return;
     }
+
+	$dataType.Increment = $attr.Increment;
+	$dataType.IncrementFunction = $attr.IncrementFunction;
 }
 
-function ApplyUnit([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyUnit([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.UnitAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.UnitAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.Unit = $attr.Unit;
+		return;
     }
+	
+	$dataType.Unit = $attr.Unit;
 }
 
-function ApplyValidateSet([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyValidateSet([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidateSetIfRequiredAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidateSetIfNotDefaultAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.ValidateSet = $attr.Set;
+		return;
     }
+
+	$dataType.ValidateSet = $attr.Set;
 }
 
-function ApplyValidateScript([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyValidateScript([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidateScriptIfRequiredAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidateScriptIfNotDefaultAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.ValidateScript = $attr.Script;
+		return;
     }
+	
+	$dataType.ValidateScript = $attr.Script;
 }
 
-function ApplyValidatePattern([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType]$dataType, $property)
+function ApplyValidatePattern([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
 {
-    $attr = $classProperty.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidatePatternIfRequiredAttribute], $true);
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.ValidatePatternIfNotDefaultAttribute], $true);
 
-    if ($attr)
+    if (!$attr)
     {
-        $dataType.ValidatePattern = $attr.Pattern;
+		return;
     }
+	
+	$dataType.ValidatePattern = $attr.Pattern;
+}
+
+function ApplyKeyNameValueFilter([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
+{
+    $attr = $property.GetCustomAttributes([biz.dfch.CS.Appclusive.Public.Configuration.KeyNameValueFilterAttribute], $true);
+
+    if (!$attr)
+    {
+		return;
+    }
+	
+	$dataType.KeyNameValueFilter = $attr.Pattern;
+}
+
+function ApplyStringLength([biz.dfch.CS.Appclusive.Core.OdataServices.Diagnostics.DataType] $dataType, $property)
+{
+    $attr = $property.GetCustomAttributes([System.ComponentModel.DataAnnotations.StringLengthAttribute], $true);
+
+    if (!$attr)
+    {
+		return;
+    }
+
+	$dataType.Maximum = $attr.MaximumLength;
+	$dataType.Minimum = $attr.MinimumLength;
 }
 
 #
