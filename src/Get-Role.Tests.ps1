07c6357b75d8ac1c4f0075d88d2c091e3aec2338
rufer7 <marc.rufer@d-fens.net>
2016-11-09 10:31:20 +0100

    Get-Role.Tests
    * Additional assertion added

--- src/Get-Role.Tests.ps1
+++ src/Get-Role.Tests.ps1
@@ -217,6 +217,7 @@ Describe "Get-Role" -Tags "Get-Role" {
 			$result | Should Not Be $null;
 			$result -is [array] | Should Be $true;
 			$result.Count -gt 0 | Should Be $true;
+			$result[0] -is [biz.dfch.CS.Appclusive.Api.Core.Permission] | Should Be $true;
 		}
 	}
 }
