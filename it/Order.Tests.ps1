73272c4e6d10e0e34253b67ab59954d0f0a07cef
Paschalis Bimpisidis <pbimpisidis@gmail.com>
2016-11-11 17:06:00 +0100

    *added new test
    #112

--- it/Order.Tests.ps1
+++ it/Order.Tests.ps1
@@ -291,6 +291,61 @@ Describe "Order.Tests" -Tags "Order.Tests" {
 			$orderJobRefreshed = Get-ApcJob -id $orderJob.Id
 			$orderJobRefreshed.Status | Should Be "Cancelled";
 		}
+		
+		It "Order-OverwriteParameters" -Test {
+			#ARRANGE
+			$catalogueName = $entityPrefix + "Catalogue";
+			$productName = $entityPrefix + "Product";
+			$catalogueItemName = $entityPrefix + "CatalogueItem";
+			$cartItemName = $entityPrefix + "CartItem";
+			$orderName = $entityPrefix + "Order";
+			
+			$productParameters = '{"A":"OLD", "B":"OLD"}';
+			$catalogueItemParameters = '{"A":"NEW", "C":"OLD"}';
+			
+			#ACT create catalogue
+			$newCatalogue = Create-Catalogue -svc $svc -name $catalogueName;
+			$catalogueId = $newCatalogue.Id;
+			
+			#ACT create product
+			$newProduct = Create-Product -svc $svc -name $productName -Parameters $productParameters;
+			$productId = $newProduct.Id;
+			
+			#ACT create catalogue item
+			$newCatalogueItem = Create-CatalogueItem -svc $svc -name $catalogueItemName -catalogueId $catalogueId -productId $productId -Parameters $catalogueItemParameters;
+			$catalogueItemId = $newCatalogueItem.Id;
+			
+			#ACT create new cart item
+			$cartItem = Create-CartItem -svc $svc -Name $cartItemName -CatalogueItemId $catalogueItemId;
+			$cartItemId = $cartItem.Id;
+			$cartId = $cartItem.CartId;
+			
+			#get cart
+			$query = "Id eq {0}" -f $cartId;
+			$cart = $svc.Core.Carts.AddQueryOption('$filter', $query) | Select;
+			
+			#ASSERT cart
+			$cartItems = $svc.Core.LoadProperty($cart, 'CartItems') | Select;
+			$cartItems.Count | Should Be 1;
+			$cartItems[0].Id | Should Be $cartItemId;
+			
+			#ACT create order
+			$orderParameters = @{
+				Name = $orderName;
+				Description = "Arbitrary Description";
+				Requester = (Get-ApcUser -Current).Id;
+				Parameters = '{}';
+			}
+			
+			$createOrder = $svc.Core.InvokeEntitySetActionWithSingleResult("Orders", "Create",  [biz.dfch.CS.Appclusive.Api.Core.Order], $orderParameters );
+			
+			Start-Sleep -s 5;
+			
+			#ASSERT get the cart and check that it is deleted
+			$query = "Id eq {0}" -f $cartId;
+			$cart = $svc.Core.Carts.AddQueryOption('$filter', $query) | Select;
+			$cart | Should Be $null;
+		}
 	}
 }
 
